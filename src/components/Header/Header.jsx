74cc7d9dbb55e27e417f981d3af4a8e6f60e3b7d
Nathaniel Daley <ndaley64@gmail.com>
2025-01-12 17:25:57 -0600

    add and style basic navigation bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,12 @@
+import Navigation from "../Navigation/Navigation";
+
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="header__title">NewsExplorer</div>
+      <Navigation></Navigation>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="navigation">
+      <div className="navigation__link">Home</div>
+      <button className="navigation__button">Sign in</button>
+    </div>
+  );
+}
+
+export default Navigation;
