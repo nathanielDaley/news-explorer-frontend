4fd6f64dd11e70769cf8717b9a09bd4271773f62
Nathaniel Daley <ndaley64@gmail.com>
2025-01-12 17:04:33 -0600

    initial pass at styling and positioning header

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
+import Header from "../Header/Header";
+import background from "../../assets/search-background.jpg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <div className="app__content">
-        <div className="app__container">test</div>
+        <div className="app__container">
+          <Header></Header>
+          <img src={background} alt="test" className="test" />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__title">NewsExplorer</div>
+    </div>
+  );
+}
+
+export default Header;
