72d55af9b7c50804a651ee4cf463dedd6b6c84cf
Nathaniel Daley <ndaley64@gmail.com>
2025-01-12 16:29:47 -0600

    basic app boilerplate and styling

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <>test</>;
+  return (
+    <div className="app">
+      <div className="app__content">
+        <div className="app__container">test</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
