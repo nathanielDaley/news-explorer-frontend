41d5c77b25d9e311d30fa5ad0278aca76891545d
Nathaniel Daley <ndaley64@gmail.com>
2025-01-12 19:07:10 -0600

    update header styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,10 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="navigation">
-      <div className="navigation__link">Home</div>
+      <div className="navigation__link navigation__link_selected">Home</div>
+      <div className="navigation__link navigation__link_hidden">
+        Saved articles
+      </div>
       <button className="navigation__button">Sign in</button>
     </div>
   );
