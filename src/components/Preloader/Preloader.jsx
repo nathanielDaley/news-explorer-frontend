db34fccae3666cb4f33fdb24ed9bd6174b1e207d
Nathaniel Daley <ndaley64@gmail.com>
2025-01-22 13:58:11 -0600

    show main hide profile switch header colors

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,7 +2,7 @@ import "./Preloader.css";
 
 function Preloader() {
   return (
-    <section className="preloader">
+    <section className="preloader preloader_hidden">
       <div className="preloader__spinner"></div>
       <div className="preloader__title">Searching for news...</div>
     </section>
