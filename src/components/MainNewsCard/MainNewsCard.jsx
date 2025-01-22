98c42cc81464c2a39bf335ec92373e67d094f778
Nathaniel Daley <ndaley64@gmail.com>
2025-01-22 16:40:42 -0600

    clicking news card navigates to article in new tab

--- src/components/MainNewsCard/MainNewsCard.jsx
+++ src/components/MainNewsCard/MainNewsCard.jsx
@@ -6,24 +6,40 @@ import savedIcon from "../../assets/saved.svg";
 import unsavedIcon from "../../assets/unsaved.svg";
 
 function MainNewsCard({ newsArticle }) {
+  const handleSaveClick = (event) => {
+    event.preventDefault();
+
+    console.log("test");
+  };
+
   return (
     <article className="main-news-card">
-      <button type="button" className="main-news-card__save-button">
-        <img
-          src={unsavedIcon}
-          alt="unsaved icon"
-          className="main-news-card__unsaved-icon"
-        />
-        <img
-          src={savedIcon}
-          alt="saved icon"
-          className="main-news-card__saved-icon main-news-card__icon_hidden"
-        />
-      </button>
-      <label htmlFor="" className="main-news-card__save-button-description">
-        Sign in to save articles
-      </label>
-      {<NewsCard newsArticle={newsArticle}></NewsCard>}
+      <a
+        href={newsArticle.url}
+        target="_blank"
+        className="main-news-card__card-link"
+      >
+        <button
+          type="button"
+          onClick={handleSaveClick}
+          className="main-news-card__save-button"
+        >
+          <img
+            src={unsavedIcon}
+            alt="unsaved icon"
+            className="main-news-card__unsaved-icon"
+          />
+          <img
+            src={savedIcon}
+            alt="saved icon"
+            className="main-news-card__saved-icon main-news-card__icon_hidden"
+          />
+        </button>
+        <label htmlFor="" className="main-news-card__save-button-description">
+          Sign in to save articles
+        </label>
+        {<NewsCard newsArticle={newsArticle}></NewsCard>}
+      </a>
     </article>
   );
 }
--- src/components/SavedNewsCard/SavedNewsCard.jsx
+++ src/components/SavedNewsCard/SavedNewsCard.jsx
@@ -7,18 +7,27 @@ import deleteIcon from "../../assets/delete.svg";
 function SavedNewsCard({ newsArticle }) {
   return (
     <article className="saved-news-card">
-      <label className="saved-news-card__keyword">Nature</label>
-      <button type="button" className="saved-news-card__delete-button">
-        <img
-          src={deleteIcon}
-          alt="delete icon"
-          className="saved-news-card__delete-icon"
-        />
-      </button>
-      <label htmlFor="" className="saved-news-card__delete-button-description">
-        Remove from saved
-      </label>
-      {<NewsCard newsArticle={newsArticle}></NewsCard>}
+      <a
+        href={newsArticle.url}
+        target="_blank"
+        className="main-news-card__card-link"
+      >
+        <label className="saved-news-card__keyword">Nature</label>
+        <button type="button" className="saved-news-card__delete-button">
+          <img
+            src={deleteIcon}
+            alt="delete icon"
+            className="saved-news-card__delete-icon"
+          />
+        </button>
+        <label
+          htmlFor=""
+          className="saved-news-card__delete-button-description"
+        >
+          Remove from saved
+        </label>
+        {<NewsCard newsArticle={newsArticle}></NewsCard>}
+      </a>
     </article>
   );
 }
