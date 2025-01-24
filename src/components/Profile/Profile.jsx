682b0d454b84afe79c97a69ac8e2eacb94dd36e0
Nathaniel Daley <ndaley64@gmail.com>
2025-01-24 17:14:46 -0600

    get profile user name from user context

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,17 @@
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+
 import SavedNewsCardList from "../SavedNewsCardList/SavedNewsCardList";
 import "./Profile.css";
 
 function Profile({ newsArticles }) {
+  const { currentUser, isLoggedIn } = useContext(CurrentUserContext);
+
   return (
     <main className="profile">
       <div className="profile__header">
         <h3 className="profile__title">Saved articles</h3>
         <h2 className="profile__information">
-          Elise, you have 5 saved articles
+          {currentUser.name}, you have 5 saved articles
         </h2>
         <p className="profile__keywords-paragraph">
           By keywords:{" "}
