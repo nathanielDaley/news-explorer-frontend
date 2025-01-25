a727e0e0dacfd29f77cde33dbd75a28fae553647
Nathaniel Daley <ndaley64@gmail.com>
2025-01-24 22:47:15 -0600

    update about description

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,11 +9,17 @@ function About() {
       <div className="about__content">
         <h2 className="about__title">About the author</h2>
         <p className="about__description">
-          This block describes the project author. Here you should indicate your
-          name, what you do, and which development technologies you know.
+          I’m Nathaniel Daley, a Product Test Engineer with expertise in quality
+          assurance, test automation, and system reliability, focusing on IV&V
+          and Agile methodologies. I’m skilled in Python, Linux systems, and
+          various programming languages, allowing me to efficiently handle
+          complex software and hardware testing.
           <br />
-          <br /> You can also talk about your experience with TripleTen, what
-          you learned there, and how you can help potential customers.
+          <br />
+          Outside of work, I love spending time with my dog and playing video
+          games, which helps me hone my problem-solving and strategic thinking
+          skills. My combination of technical expertise and personal passions
+          makes me a well-rounded and driven individual.
         </p>
       </div>
     </section>
